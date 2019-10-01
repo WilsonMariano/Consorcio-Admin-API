13fb2c7546361f24e6e6985d11a924b7ed0912ed
WilsonMariano <mgw009@gmail.com>
2019-10-01 16:56:57 -0300

    Cambios para heroku

--- index.php
+++ index.php
@@ -20,9 +20,9 @@
     $app->add(function ($req, $res, $next){
 		$response = $next($req, $res);
 		return $response
-			->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+		->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 	});
 
 
