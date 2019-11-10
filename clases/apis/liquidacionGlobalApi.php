e3c0c0722042f55003b90002a139ba30c24a058f
Pablo Valenzuela <pablo86v@gmail.com>
2019-11-10 14:09:52 -0300

    change calendar function

--- clases/apis/liquidacionGlobalApi.php
+++ clases/apis/liquidacionGlobalApi.php
@@ -19,7 +19,8 @@ class LiquidacionGlobalApi{
         // Si el mes es diciembre, calcular como enero. Sino, usar mes siguiente
         $mes = $liquidacionGbl->mes == 12 ? 1 : ($liquidacionGbl->mes + 1);
         $anio = $liquidacionGbl->anio;
-        $CantDiasMes = days_in_month($mes, $anio); 
+        // $CantDiasMes = days_in_month($mes, $anio); 
+        $CantDiasMes = cal_days_in_month(CAL_GREGORIAN, $mes, $anio); 
 
         for($i=$CantDiasMes; $i>0; $i--){
             $nroDia =  jddayofweek (gregoriantojd($mes, $i, $anio), 0);
