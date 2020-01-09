e5f86ab965d57cd659bff431970de2a82fb978a8
Pablo Valenzuela <pablo86v@gmail.com>
2020-01-08 23:52:43 -0300

    + NC/ND

---
+++ clases/apis/notaCreditoDebitoApi.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once __DIR__ . '/../NotasDebito.php';
+include_once __DIR__ . '/../CtasCtes.php';
+include_once __DIR__ . '/../UF.php';
+include_once __DIR__ . '/../_FuncionesEntidades.php';
+
+class NotaCreditoDebitoApi{
+    
+    public static function New($request, $response, $args){
+        $apiParams = $request->getParsedBody();
+        
+        if($apiParams['tipoDocumento'] == 'NC'){
+            // todo: elegir segun el tipo de entidad
+        }else{
+
+        }
+   
+        if(!UF::IsDuplicated($objUF))
+            if(Funciones::InsertOne($objUF, true) > 0)
+                return $response->withJson(true, 200);
+            else
+                return $response->withJson(false, 400);
+        else 
+            return $response->withJson("El nro de unidad funcional ingresado no se encuentra disponible.", 400);
+    }
+    
+}//class
